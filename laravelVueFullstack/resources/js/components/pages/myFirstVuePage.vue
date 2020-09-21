d5611bf6f68fa75054cd06a355c3da69ff499cec
dzulfikriAlfik <fikrialfik@gmail.com>
2020-09-21 19:01:19 +0700

    delete folder laravel, back to basic

--- laravelVueFullstack/resources/js/components/pages/myFirstVuePage.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>This is our first page...</h1>
-    </div>
-</template>
--- laravelVueFullstack/resources/js/components/pages/newRoutePage.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>This is second new page</h1>
-    </div>
-</template>
