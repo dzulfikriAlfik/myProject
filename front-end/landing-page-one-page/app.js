41a2aa0694844b04caa452acd4e3d657e6e0fca3
dzulfikriAlfik <fikriallfik@gmail.com>
2022-01-20 15:41:48 +0700

    update

--- front-end/landing-page-one-page/app.js
+++ front-end/landing-page-one-page/app.js
@@ -1,25 +1,29 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobileMenu = document.querySelector('.header .nav-bar .nav-list ul');
-const menuItem = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container')
+const hamburger = document.querySelector(
+   ".header .nav-bar .nav-list .hamburger"
+);
+const mobileMenu = document.querySelector(".header .nav-bar .nav-list ul");
+const menuItem = document.querySelectorAll(
+   ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
 
-hamburger.addEventListener('click', () => {
-   hamburger.classList.toggle('active');
-   mobileMenu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+   hamburger.classList.toggle("active");
+   mobileMenu.classList.toggle("active");
 });
 
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
    let scrollPosition = window.scrollY;
    if (scrollPosition > 100) {
-      header.style.backgroundColor = '#29323c'
+      header.style.backgroundColor = "#29323c";
    } else {
-      header.style.backgroundColor = 'transparent'
+      header.style.backgroundColor = "transparent";
    }
 });
 
-menuItem.forEach(item => {
-   item.addEventListener('click', () => {
-      hamburger.classList.toggle('active');
-      mobileMenu.classList.toggle('active');
-   })
-})
+menuItem.forEach((item) => {
+   item.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      mobileMenu.classList.toggle("active");
+   });
+});
