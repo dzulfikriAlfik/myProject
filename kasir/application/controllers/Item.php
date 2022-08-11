2ca6f18d2c128e5d6d3121c6574a1096a0a9d57a
dzulfikriAlfik <fikriallfik@gmail.com>
2022-08-12 06:23:40 +0700

    update

--- kasir/application/controllers/Item.php
+++ kasir/application/controllers/Item.php
@@ -4,210 +4,210 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Item extends CI_Controller
 {
 
-   public function __construct()
-   {
-      parent::__construct();
-      $this->load->model(['item_model', 'category_model', 'unit_model']);
-      cek_not_login();
-   }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model(['item_model', 'category_model', 'unit_model']);
+		cek_not_login();
+	}
 
-   public function index()
-   {
-      $data = [
-         'row'    => $this->item_model->get()->result_array(),
-         'aktif'  => 'item',
-         'menu'   => 'item',
-      ];
-      $this->template->load('template', 'Product/Item/item_data', $data);
-   }
+	public function index()
+	{
+		$data = [
+			'row'    => $this->item_model->get()->result_array(),
+			'aktif'  => 'item',
+			'menu'   => 'item',
+		];
+		$this->template->load('template', 'Product/Item/item_data', $data);
+	}
 
-   public function add()
-   {
-      $item = new stdClass();
-      $item->item_id       = set_value('item_id');
-      $item->category_id   = set_value('category_id');
-      $item->barcode       = set_value('barcode');
-      $item->name          = set_value('name');
-      $item->price         = set_value('price');
-      $item->image         = set_value('image');
+	public function add()
+	{
+		$item = new stdClass();
+		$item->item_id       = set_value('item_id');
+		$item->category_id   = set_value('category_id');
+		$item->barcode       = set_value('barcode');
+		$item->name          = set_value('name');
+		$item->price         = set_value('price');
+		$item->image         = set_value('image');
 
-      $query_category   = $this->category_model->get();
-      $query_unit       = $this->unit_model->get();
-      $unit[null]       = '-- Pilih Unit --';
-      foreach ($query_unit->result() as $unt) {
-         $unit[$unt->unit_id] = $unt->name;
-      }
+		$query_category   = $this->category_model->get();
+		$query_unit       = $this->unit_model->get();
+		$unit[null]       = '-- Pilih Unit --';
+		foreach ($query_unit->result() as $unt) {
+			$unit[$unt->unit_id] = $unt->name;
+		}
 
-      $data = [
-         'aktif'           => 'item',
-         'menu'            => 'item',
-         'page'            => 'add',
-         'row'             => $item,
-         'category'        => $query_category,
-         'unit'            => $unit,
-         'selected_unit'   => null
-      ];
-      $this->template->load('template', 'Product/Item/item_form', $data);
-   }
+		$data = [
+			'aktif'           => 'item',
+			'menu'            => 'item',
+			'page'            => 'add',
+			'row'             => $item,
+			'category'        => $query_category,
+			'unit'            => $unit,
+			'selected_unit'   => null
+		];
+		$this->template->load('template', 'Product/Item/item_form', $data);
+	}
 
-   public function edit($id)
-   {
-      $query = $this->item_model->get($id);
-      if ($query->num_rows() > 0) {
-         $item             = $query->row();
+	public function edit($id)
+	{
+		$query = $this->item_model->get($id);
+		if ($query->num_rows() > 0) {
+			$item             = $query->row();
 
-         $query_category   = $this->category_model->get();
-         $query_unit       = $this->unit_model->get();
-         $unit[null]       = '-- Pilih Unit --';
-         foreach ($query_unit->result() as $unt) {
-            $unit[$unt->unit_id] = $unt->name;
-         }
-         $data = [
-            'aktif'              => 'item',
-            'menu'               => 'item',
-            'page'               => 'edit',
-            'row'                => $item,
-            'category'           => $query_category,
-            'unit'               => $unit,
-            'selected_category'  => $item->category_id,
-            'selected_unit'      => $item->unit_id
-         ];
-         $this->template->load('template', 'Product/Item/item_form', $data);
-      } else {
-         pesan_error('Tidak Ditemukan', 'item');
-      }
-   }
+			$query_category   = $this->category_model->get();
+			$query_unit       = $this->unit_model->get();
+			$unit[null]       = '-- Pilih Unit --';
+			foreach ($query_unit->result() as $unt) {
+				$unit[$unt->unit_id] = $unt->name;
+			}
+			$data = [
+				'aktif'              => 'item',
+				'menu'               => 'item',
+				'page'               => 'edit',
+				'row'                => $item,
+				'category'           => $query_category,
+				'unit'               => $unit,
+				'selected_category'  => $item->category_id,
+				'selected_unit'      => $item->unit_id
+			];
+			$this->template->load('template', 'Product/Item/item_form', $data);
+		} else {
+			pesan_error('Tidak Ditemukan', 'item');
+		}
+	}
 
-   public function process()
-   {
-      $post = $this->input->post(null, TRUE);
-      $data['menu'] = 'item';
+	public function process()
+	{
+		$post = $this->input->post(null, TRUE);
+		$data['menu'] = 'item';
 
-      $config['upload_path']    = './uploads/product/';
-      $config['allowed_types']  = 'gif|jpg|png|jpeg';
-      $config['max_size']       = 2048;
-      $config['file_name']      = "item-$post[product_name]-" . date('dmY') . '-' . substr(md5(rand()), 0, 10);
-      $this->load->library('upload', $config);
+		$config['upload_path']    = './uploads/product/';
+		$config['allowed_types']  = 'gif|jpg|png|jpeg';
+		$config['max_size']       = 2048;
+		$config['file_name']      = "item-$post[product_name]-" . date('dmY') . '-' . substr(md5(rand()), 0, 10);
+		$this->load->library('upload', $config);
 
-      // ADD
-      if (isset($_POST['add'])) {
-         if ($this->item_model->check_barcode($post['barcode'])->num_rows() > 0) {
-            pesan_error("Barcode $post[barcode] sudah dipakai barang lain", 'item/add');
-         } else {
-            if (@$_FILES['image']['name'] != null) {
-               if ($this->upload->do_upload('image')) {
-                  $post['image'] = $this->upload->data('file_name');
-                  $this->item_model->add($post);
-                  pesan_alert('Ditambahkan', 'item');
-               } else {
-                  $error = $this->upload->display_errors();
-                  pesan_error('Gagal Upload', 'item/add');
-               }
-            } else {
-               $post['image'] = null;
-               $this->item_model->add($post);
-               pesan_alert('ditambahkan', 'item');
-            }
-         }
-         // EDIT
-      } else if (isset($_POST['edit'])) {
-         if ($this->item_model->check_barcode($post['barcode'], $post['item_id'])->num_rows() > 0) {
-            pesan_error("Barcode $post[barcode] sudah dipakai barang lain", "item/edit/$post[item_id]");
-         } else {
-            if (@$_FILES['image']['name'] != null) {
-               if ($this->upload->do_upload('image')) {
-                  $item = $this->item_model->get($post['item_id'])->row();
-                  if ($item->image != null) {
-                     $target_file   = "./uploads/product/$item->image";
-                     unlink($target_file);
-                  }
+		// ADD
+		if (isset($_POST['add'])) {
+			if ($this->item_model->check_barcode($post['barcode'])->num_rows() > 0) {
+				pesan_error("Barcode $post[barcode] sudah dipakai barang lain", 'item/add');
+			} else {
+				if (@$_FILES['image']['name'] != null) {
+					if ($this->upload->do_upload('image')) {
+						$post['image'] = $this->upload->data('file_name');
+						$this->item_model->add($post);
+						pesan_alert('Ditambahkan', 'item');
+					} else {
+						$error = $this->upload->display_errors();
+						pesan_error('Gagal Upload', 'item/add');
+					}
+				} else {
+					$post['image'] = null;
+					$this->item_model->add($post);
+					pesan_alert('ditambahkan', 'item');
+				}
+			}
+			// EDIT
+		} else if (isset($_POST['edit'])) {
+			if ($this->item_model->check_barcode($post['barcode'], $post['item_id'])->num_rows() > 0) {
+				pesan_error("Barcode $post[barcode] sudah dipakai barang lain", "item/edit/$post[item_id]");
+			} else {
+				if (@$_FILES['image']['name'] != null) {
+					if ($this->upload->do_upload('image')) {
+						$item = $this->item_model->get($post['item_id'])->row();
+						if ($item->image != null) {
+							$target_file   = "./uploads/product/$item->image";
+							unlink($target_file);
+						}
 
-                  $post['image'] = $this->upload->data('file_name');
-                  $this->item_model->edit($post);
-                  pesan_alert('Diupdate', 'item');
-               } else {
-                  $error = $this->upload->display_errors();
-                  pesan_error('Gagal Upload', 'item/add');
-               }
-            } else {
-               $post['image'] = null;
-               $this->item_model->edit($post);
-               pesan_alert('Diupdate', 'item');
-            }
-         }
-      }
-   }
+						$post['image'] = $this->upload->data('file_name');
+						$this->item_model->edit($post);
+						pesan_alert('Diupdate', 'item');
+					} else {
+						$error = $this->upload->display_errors();
+						pesan_error('Gagal Upload', 'item/add');
+					}
+				} else {
+					$post['image'] = null;
+					$this->item_model->edit($post);
+					pesan_alert('Diupdate', 'item');
+				}
+			}
+		}
+	}
 
-   public function delete($id)
-   {
-      $item = $this->item_model->get($id)->row();
-      if ($item->image != null) {
-         $target_file   = "./uploads/product/$item->image";
-         unlink($target_file);
-      }
-      $item_barcode  = "./uploads/qr-code/$item->barcode-Item-" . getnama($item->item_id, 'p_item', 'item_id', 'name') . ".png";
-      unlink($item_barcode);
+	public function delete($id)
+	{
+		$item = $this->item_model->get($id)->row();
+		if ($item->image != null) {
+			$target_file   = "./uploads/product/$item->image";
+			unlink($target_file);
+		}
+		$item_barcode  = "./uploads/qr-code/$item->barcode-Item-" . getnama($item->item_id, 'p_item', 'item_id', 'name') . ".png";
+		unlink($item_barcode);
 
-      $where = ['item_id' => $id];
-      $this->item_model->delete($where, 'p_item');
-      pesan_alert('Dihapus', 'item');
-   }
+		$where = ['item_id' => $id];
+		$this->item_model->delete($where, 'p_item');
+		pesan_alert('Dihapus', 'item');
+	}
 
-   function barcode_qrcode($id)
-   {
-      $data = [
-         'row'    => $this->item_model->get($id)->row(),
-         'aktif'  => 'item',
-         'menu'   => 'item',
-         'page'   => 'Barcode Generator',
-      ];
-      $this->template->load('template', 'Product/Item/barcode_qrcode', $data);
-   }
+	function barcode_qrcode($id)
+	{
+		$data = [
+			'row'    => $this->item_model->get($id)->row(),
+			'aktif'  => 'item',
+			'menu'   => 'item',
+			'page'   => 'Barcode Generator',
+		];
+		$this->template->load('template', 'Product/Item/barcode_qrcode', $data);
+	}
 
-   public function barcode_print($id)
-   {
-      $data['row'] = $this->item_model->get($id)->row();
-      $html = $this->load->view('Product/Item/barcode_print', $data, true);
+	public function barcode_print($id)
+	{
+		$data['row'] = $this->item_model->get($id)->row();
+		$html = $this->load->view('Product/Item/barcode_print', $data, true);
 
-      $this->fungsi->PdfGenerator($html, 'Barcode-' . $data['row']->barcode, 'A4', 'Portrait');
-   }
+		$this->fungsi->PdfGenerator($html, 'Barcode-' . $data['row']->barcode, 'A4', 'Portrait');
+	}
 
-   public function qrcode_print($id)
-   {
-      $data['row'] = $this->item_model->get($id)->row();
-      $html = $this->load->view('Product/Item/qrcode_print', $data, true);
+	public function qrcode_print($id)
+	{
+		$data['row'] = $this->item_model->get($id)->row();
+		$html = $this->load->view('Product/Item/qrcode_print', $data, true);
 
-      $this->fungsi->PdfGenerator($html, 'QRCode-' . $data['row']->barcode, 'A4', 'Landscape');
-   }
+		$this->fungsi->PdfGenerator($html, 'QRCode-' . $data['row']->barcode, 'A4', 'Landscape');
+	}
 
-   function get_ajax()
-   {
-      $list = $this->item_model->get_datatables();
-      $data = array();
-      $no = @$_POST['start'];
-      foreach ($list as $item) {
-         $no++;
-         $row = array();
-         $row[] = $no . ".";
-         $row[] = $item->barcode . '<br><a href="' . site_url('item/barcode_qrcode/' . $item->item_id) . '" class="btn btn-default btn-xs">Generate <i class="fa fa-barcode"></i></a>';
-         $row[] = $item->name;
-         $row[] = $item->category_name;
-         $row[] = $item->unit_name;
-         $row[] = rupiah($item->price);
-         $row[] = $item->stock;
-         $row[] = $item->image != null ? '<img src="' . base_url('uploads/product/' . $item->image) . '" class="img" style="width:100px">' : 'uploads/product/product_default.jpg';
-         // add html for action
-         $row[] = '<a href="' . site_url('item/edit/' . $item->item_id) . '" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i> Update</a>
+	function get_ajax()
+	{
+		$list = $this->item_model->get_datatables();
+		$data = array();
+		$no = @$_POST['start'];
+		foreach ($list as $item) {
+			$no++;
+			$row = array();
+			$row[] = $no . ".";
+			$row[] = $item->barcode . '<br><a href="' . site_url('item/barcode_qrcode/' . $item->item_id) . '" class="btn btn-default btn-xs">Generate <i class="fa fa-barcode"></i></a>';
+			$row[] = $item->name;
+			$row[] = $item->category_name;
+			$row[] = $item->unit_name;
+			$row[] = rupiah($item->price);
+			$row[] = $item->stock;
+			$row[] = $item->image != null ? '<img src="' . base_url('uploads/product/' . $item->image) . '" class="img" style="width:100px">' : 'uploads/product/product_default.jpg';
+			// add html for action
+			$row[] = '<a href="' . site_url('item/edit/' . $item->item_id) . '" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i> Update</a>
                   <a href="' . site_url('item/delete/' . $item->item_id) . '" onclick="return confirm(\'Yakin hapus data?\')"  class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> Delete</a>';
-         $data[] = $row;
-      }
-      $output = array(
-         "draw" => @$_POST['draw'],
-         "recordsTotal" => $this->item_model->count_all(),
-         "recordsFiltered" => $this->item_model->count_filtered(),
-         "data" => $data,
-      );
-      // output to json format
-      echo json_encode($output);
-   }
+			$data[] = $row;
+		}
+		$output = array(
+			"draw" => @$_POST['draw'],
+			"recordsTotal" => $this->item_model->count_all(),
+			"recordsFiltered" => $this->item_model->count_filtered(),
+			"data" => $data,
+		);
+		// output to json format
+		echo json_encode($output);
+	}
 }
--- kasir/application/models/Sales_model.php
+++ kasir/application/models/Sales_model.php
@@ -3,138 +3,138 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Sales_model extends CI_Model
 {
-   public function invoice_no()
-   {
-      $sql     = "SELECT MAX(MID(invoice,12,4)) AS invoice_no 
+	public function invoice_no()
+	{
+		$sql     = "SELECT MAX(MID(invoice,12,4)) AS invoice_no 
                   FROM t_sales 
                   WHERE MID(invoice,6,6) = DATE_FORMAT(CURDATE(), '%y%m%d') ";
-      $query   = $this->db->query($sql);
+		$query   = $this->db->query($sql);
 
-      if ($query->num_rows() > 0) {
-         $row     = $query->row();
-         $n       = ((int)$row->invoice_no) + 1;
-         $no      = sprintf("%'.04d", $n);
-      } else {
-         $no = "0001";
-      }
-      $invoice = "MYKSR" . date('ymd') . $no;
-      return $invoice;
-   }
+		if ($query->num_rows() > 0) {
+			$row     = $query->row();
+			$n       = ((int) $row->invoice_no) + 1;
+			$no      = sprintf("%'.04d", $n);
+		} else {
+			$no = "0001";
+		}
+		$invoice = "MYKSR" . date('ymd') . $no;
+		return $invoice;
+	}
 
-   public function get_cart($params = null)
-   {
-      $this->db->select('*, p_item.name as item_name, t_cart.price as cart_price');
-      $this->db->from('t_cart');
-      $this->db->join('p_item', 't_cart.item_id = p_item.item_id');
-      if ($params != null) {
-         $this->db->where($params);
-      }
-      $this->db->where('user_id', $this->session->userdata('userid'));
-      return $this->db->get();
-   }
+	public function get_cart($params = null)
+	{
+		$this->db->select('*, p_item.name as item_name, t_cart.price as cart_price');
+		$this->db->from('t_cart');
+		$this->db->join('p_item', 't_cart.item_id = p_item.item_id');
+		if ($params != null) {
+			$this->db->where($params);
+		}
+		$this->db->where('user_id', $this->session->userdata('userid'));
+		return $this->db->get();
+	}
 
-   public function add_cart($post)
-   {
-      $query = $this->db->query("SELECT MAX(cart_id) AS cart_no FROM t_cart");
-      if ($query->num_rows() > 0) {
-         $row = $query->row();
-         $car_no = ((int)$row->cart_no) + 1;
-      } else {
-         $car_no = "1";
-      }
+	public function add_cart($post)
+	{
+		$query = $this->db->query("SELECT MAX(cart_id) AS cart_no FROM t_cart");
+		if ($query->num_rows() > 0) {
+			$row = $query->row();
+			$car_no = ((int) $row->cart_no) + 1;
+		} else {
+			$car_no = "1";
+		}
 
-      $params = [
-         'cart_id' => $car_no,
-         'item_id' => $post['item_id'],
-         'price' => $post['price'],
-         'qty' => $post['qty'],
-         'total' => ($post['price'] * $post['qty']),
-         'user_id' => $this->session->userdata('userid')
-      ];
-      $this->db->insert('t_cart', $params);
-   }
+		$params = [
+			'cart_id' => $car_no,
+			'item_id' => $post['item_id'],
+			'price' => $post['price'],
+			'qty' => $post['qty'],
+			'total' => ($post['price'] * $post['qty']),
+			'user_id' => $this->session->userdata('userid')
+		];
+		$this->db->insert('t_cart', $params);
+	}
 
-   public function update_cart_qty($post)
-   {
-      $query = "UPDATE t_cart SET price = '$post[price]', 
+	public function update_cart_qty($post)
+	{
+		$query = "UPDATE t_cart SET price = '$post[price]', 
                qty = qty + '$post[qty]',
                total = '$post[price]' * qty
                WHERE item_id = '$post[item_id]' ";
-      $this->db->query($query);
-   }
+		$this->db->query($query);
+	}
 
-   public function delete_cart($params = null)
-   {
-      if ($params != null) {
-         $this->db->where($params);
-      }
-      $this->db->delete('t_cart');
-   }
+	public function delete_cart($params = null)
+	{
+		if ($params != null) {
+			$this->db->where($params);
+		}
+		$this->db->delete('t_cart');
+	}
 
-   public function edit_cart($post)
-   {
-      $params = [
-         'price'           => $post['price'],
-         'qty'             => $post['qty'],
-         'discount_item'   => $post['discount'],
-         'total'           => $post['total']
-      ];
-      $this->db->where('cart_id', $post['cart_id']);
-      $this->db->update('t_cart', $params);
-   }
+	public function edit_cart($post)
+	{
+		$params = [
+			'price'           => $post['price'],
+			'qty'             => $post['qty'],
+			'discount_item'   => $post['discount'],
+			'total'           => $post['total']
+		];
+		$this->db->where('cart_id', $post['cart_id']);
+		$this->db->update('t_cart', $params);
+	}
 
-   public function add_sale($post)
-   {
-      $params = [
-         'invoice'      => $this->invoice_no(),
-         'customer_id'  => $post['customer_id'] == '' ? null : $post['customer_id'],
-         'total_price'  => $post['subtotal'],
-         'discount'     => $post['discount'],
-         'final_price'  => $post['grandtotal'],
-         'cash'         => $post['cash'],
-         'remaining'    => $post['change'],
-         'note'         => $post['note'],
-         'date'         => $post['date'],
-         'user_id'      => $this->session->userdata('userid')
-      ];
-      $this->db->insert('t_sales', $params);
-      return $this->db->insert_id();
-   }
+	public function add_sale($post)
+	{
+		$params = [
+			'invoice'      => $this->invoice_no(),
+			'customer_id'  => $post['customer_id'] == '' ? null : $post['customer_id'],
+			'total_price'  => $post['subtotal'],
+			'discount'     => $post['discount'],
+			'final_price'  => $post['grandtotal'],
+			'cash'         => $post['cash'],
+			'remaining'    => $post['change'],
+			'note'         => $post['note'],
+			'date'         => $post['date'],
+			'user_id'      => $this->session->userdata('userid')
+		];
+		$this->db->insert('t_sales', $params);
+		return $this->db->insert_id();
+	}
 
-   function add_sale_detail($params)
-   {
-      $this->db->insert_batch('t_sales_detail', $params);
-   }
+	function add_sale_detail($params)
+	{
+		$this->db->insert_batch('t_sales_detail', $params);
+	}
 
-   public function get_sales($id = null)
-   {
-      $this->db->select('*, customer.name as customer_name, user.name as user_name, t_sales.created as sales_created');
-      $this->db->from('t_sales');
-      $this->db->join('customer', 't_sales.customer_id = customer.customer_id', 'left');
-      $this->db->join('user', 't_sales.user_id = user.user_id');
-      if ($id != null) {
-         $this->db->where('sales_id', $id);
-      }
+	public function get_sales($id = null)
+	{
+		$this->db->select('*, customer.name as customer_name, user.name as user_name, t_sales.created as sales_created');
+		$this->db->from('t_sales');
+		$this->db->join('customer', 't_sales.customer_id = customer.customer_id', 'left');
+		$this->db->join('user', 't_sales.user_id = user.user_id');
+		if ($id != null) {
+			$this->db->where('sales_id', $id);
+		}
 
-      $this->db->order_by('date', 'desc');
-      $query = $this->db->get();
-      return $query;
-   }
+		$this->db->order_by('date', 'desc');
+		$query = $this->db->get();
+		return $query;
+	}
 
-   public function get_sales_detail($sales_id = null)
-   {
-      $this->db->from('t_sales_detail');
-      $this->db->join('p_item', 't_sales_detail.item_id = p_item.item_id');
-      if ($sales_id != null) {
-         $this->db->where('t_sales_detail.sales_id', $sales_id);
-      }
-      $query = $this->db->get();
-      return $query;
-   }
+	public function get_sales_detail($sales_id = null)
+	{
+		$this->db->from('t_sales_detail');
+		$this->db->join('p_item', 't_sales_detail.item_id = p_item.item_id');
+		if ($sales_id != null) {
+			$this->db->where('t_sales_detail.sales_id', $sales_id);
+		}
+		$query = $this->db->get();
+		return $query;
+	}
 
-   public function delete_sales($id)
-   {
-      $this->db->where('sales_id', $id);
-      $this->db->delete('t_sales');
-   }
+	public function delete_sales($id)
+	{
+		$this->db->where('sales_id', $id);
+		$this->db->delete('t_sales');
+	}
 }
